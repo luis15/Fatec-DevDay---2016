e404d880cd7a34f6cfa82b936833bd7d613faf4d
luis15 <coelho.luisantonio@hotmail.com>
2016-04-30 16:22:31 -0300

    Inclusão do Banco

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -11,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -22,30 +24,39 @@ public class DemoApplication {
 }
 
 @RestController
-class DenunciaControlador{
-	//URLs
-	
-	@Autowired     // instancia a classe anonima (interface)
+class DenunciaControlador {
+	// URLs
+
+	@Autowired // instancia a classe anonima (interface)
 	private DenunciaRepositorio repositorio;
-	
+
 	@RequestMapping("/denuncias")
-	public List<Denuncia> buscaTodas(){
-		
+	public List<Denuncia> buscaTodas() {
 		return repositorio.findAll();
 	}
-	
-	
+
+	@RequestMapping(name = "/denuncias", method = RequestMethod.POST)
+	public void cadastra(Denuncia denuncia) {
+		repositorio.save(denuncia);
+	}
+
+	@RequestMapping(name = "/denuncias", method = RequestMethod.PUT)
+	public void atualiza(Denuncia denuncia) {
+		repositorio.save(denuncia);
+	}
+
+	@RequestMapping(name = "/denuncias", method = RequestMethod.DELETE)
+	public void remove(Denuncia denuncia) {
+		repositorio.delete(denuncia);
+	}
 }
 
-interface DenunciaRepositorio extends JpaRepository<Denuncia, Long>{
-	
-	
-	
-}
+interface DenunciaRepositorio extends JpaRepository<Denuncia, Long> {
 
+}
 
 @Entity
-class Denuncia {
+class Denuncia implements Serializable {
 	@Id
 	@GeneratedValue
 	private Long idDenuncia;
@@ -56,161 +67,111 @@ class Denuncia {
 	private String logradouro;
 	private String numero;
 	private String cep;
-	private String geoPosicao;
+	private String geo;
 	private String data;
-	private String totalDenuncia;
+	private String total;
 	private String nome;
-
-	
 
 	public Long getIdDenuncia() {
 		return idDenuncia;
 	}
 
-
-
 	public void setIdDenuncia(Long idDenuncia) {
 		this.idDenuncia = idDenuncia;
 	}
-
-
 
 	public String getIdCaso() {
 		return idCaso;
 	}
 
-
-
 	public void setIdCaso(String idCaso) {
 		this.idCaso = idCaso;
 	}
-
-
 
 	public String getEstado() {
 		return estado;
 	}
 
-
-
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-
-
 
 	public String getCidade() {
 		return cidade;
 	}
 
-
-
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
-
-
 
 	public String getBairro() {
 		return bairro;
 	}
 
-
-
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
-
-
 
 	public String getLogradouro() {
 		return logradouro;
 	}
 
-
-
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
-
-
 
 	public String getNumero() {
 		return numero;
 	}
 
-
-
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
-
-
 
 	public String getCep() {
 		return cep;
 	}
 
-
-
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
 
-
-
-	public String getGeoPosicao() {
-		return geoPosicao;
+	public String getGeo() {
+		return geo;
 	}
 
-
-
-	public void setGeoPosicao(String geoPosicao) {
-		this.geoPosicao = geoPosicao;
+	public void setGeo(String geo) {
+		this.geo = geo;
 	}
-
-
 
 	public String getData() {
 		return data;
 	}
 
-
-
 	public void setData(String data) {
 		this.data = data;
 	}
 
-
-
-	public String getTotalDenuncia() {
-		return totalDenuncia;
+	public String getTotal() {
+		return total;
 	}
 
-
-
-	public void setTotalDenuncia(String totalDenuncia) {
-		this.totalDenuncia = totalDenuncia;
+	public void setTotal(String total) {
+		this.total = total;
 	}
-
-
-
+	
 	public String getNome() {
 		return nome;
 	}
-
-
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
-
 	@Override
 	public String toString() {
-		return "Denuncia [endereco=" + endereco + "]";
-		
+		return "Denuncia [endereco=" + estado + cidade + estado + logradouro + numero + cep + data + total + geo +"]";
+
 	}
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,2 @@
-insert into Denuncia (endereco,data,nome) values ('Av 1','30/04/2016','Joao');
-insert into Denuncia (endereco,data,nome) values ('Rua 10','21/02/2016', 'Chico');
-insert into Denuncia (endereco,data,nome) values ('Av 22','14/04/2016','Pedro');
-insert into Denuncia (endereco,data,nome) values ('Rua Rodrigo Fuini','22/03/2016','Maria');
-insert into Denuncia (endereco,data,nome) values ('Av 1','11/06/2015','Cida');
+insert into Denuncia (estado,cidade,bairro,logradouro,numero,cep,data,total,geo)
+values('SP','Mogi Mirim','Centro','Rua 15','115','1380000','30-04-2016','1','23.56-76-54');
