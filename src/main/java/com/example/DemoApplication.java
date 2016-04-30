e5434ed2dafb869d93d7a4ab730ca4594f5b43c2
luis15 <coelho.luisantonio@hotmail.com>
2016-04-30 11:52:37 -0300

    Criação

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,7 +1,17 @@
 package com.example;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,4 +19,57 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+}
+
+@RestController
+class DenunciaControlador{
+	//URLs
+	
+	@Autowired     // instancia a classe anonima (interface)
+	private DenunciaRepositorio repositorio;
+	
+	@RequestMapping("/denuncias")
+	public List<Denuncia> buscaTodas(){
+		
+		return repositorio.findAll();
+	}
+	
+	
+}
+
+interface DenunciaRepositorio extends JpaRepository<Denuncia, Long>{
+	
+	
+	
+}
+
+
+@Entity
+class Denuncia {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String endereco;
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Denuncia [endereco=" + endereco + "]";
+	}
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into Denuncia (endereco) values ('Av 1');
